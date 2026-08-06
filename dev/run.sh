774f130afb4fba7c38c01504c839a536c5f63701
Daniel5756 <danya.berkovich@gmail.com>
2026-08-06 14:06:37 -0400

    created run.sh to make compiling and stuff easier. might use a makefile later but this also runs everything. so far, grub is used. thats probably fine as it simplifies many things and makes it compatible with the world. Made it so all .c files are compiled in so I can start writing different parts into other files to make life easier

---
+++ dev/run.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+#this builds 
+
+set -e
+#tell grub where do do stuff from
+export PATH="$HOME/opt/cross/bin:$PATH"
+
+echo "Building gatOS..."
+
+rm -f *.o
+
+#build grub bootloader
+i686-elf-as boot.s -o boot.o
+
+# Compile every C file in this directory
+
+for file in *.c; do
+    echo "Compiling $file"
+    i686-elf-gcc -c "$file" -o "${file%.c}.o" \
+        -std=gnu99 -ffreestanding -O2 -Wall -Wextra
+done
+
+#link boot and kernel. Can add more C files here later
+i686-elf-gcc -T linker.ld -o gatOS \
+    -ffreestanding -O2 -nostdlib *.o -lgcc
+#make sure gatOS works with grub multiboot
+if ! grub-file --is-x86-multiboot gatOS; then
+    echo "ERROR: myos is not Multiboot!"
+    exit 1 #error
+fi
+
+#where the .iso is
+#the os is named myos as in the bare bones tutorial
+mkdir -p isodir/boot/grub
+cp gatOS isodir/boot/gatOS
+cp grub.cfg isodir/boot/grub/grub.cfg
+#
+grub-mkrescue -o gatOS.iso isodir >/dev/null 2>&1
+
+
+#print stuff
+echo "Build successful."
+echo
+echo "Starting gatOS..."
+echo "To quit: press Ctrl+B, release them, then press D"
+echo
+
+
+#use tmux to escape easily
+
+tmux kill-session -t gatos 2>/dev/null || true
+
+tmux new-session -d -s gatos \
+    "qemu-system-i386 -cdrom '$PWD/gatOS.iso' -display curses"
+
+tmux attach-session -t gatos
+
+tmux kill-session -t gatos 2>/dev/null || true
+
+echo "QEMU closed."
+
+rm -f *.o
