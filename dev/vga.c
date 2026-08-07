2b1ee43f4caf7c87f0f499df914500616368d3a1
Daniel5756 <danya.berkovich@gmail.com>
2026-08-07 00:13:41 -0400

    working on vga graphic driver

---
+++ dev/vga.c
@@ -0,0 +1,81 @@
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+/* Hardware text mode color constants. */
+enum vga_color {
+	VGA_COLOR_BLACK = 0,
+	VGA_COLOR_BLUE = 1,
+	VGA_COLOR_GREEN = 2,
+	VGA_COLOR_CYAN = 3,
+	VGA_COLOR_RED = 4,
+	VGA_COLOR_MAGENTA = 5,
+	VGA_COLOR_BROWN = 6,
+	VGA_COLOR_LIGHT_GREY = 7,
+	VGA_COLOR_DARK_GREY = 8,
+	VGA_COLOR_LIGHT_BLUE = 9,
+	VGA_COLOR_LIGHT_GREEN = 10,
+	VGA_COLOR_LIGHT_CYAN = 11,
+	VGA_COLOR_LIGHT_RED = 12,
+	VGA_COLOR_LIGHT_MAGENTA = 13,
+	VGA_COLOR_LIGHT_BROWN = 14,
+	VGA_COLOR_WHITE = 15,
+};
+
+static inline uint8_t vga_color(enum vga_color c) 
+{
+	return c;
+}
+
+#define VGA_WIDTH   640
+#define VGA_WIDTH_B (640 >> 3)
+#define VGA_HEIGHT  480
+#define VGA_MEMORY  0xA0000 //pixel: 0xA0000-0xAFFFF, text: 0xB8000-0xBFFFF
+#define VGA_COLOR   0x02    //set layer
+
+size_t vga_row;
+size_t vga_column;
+volatile uint8_t* vga_buffer = (volatile uint8_t*)VGA_MEMORY;
+volatile uint8_t* vga_color  = (volatile uint8_t*)VGA_COLOR; //just index at 0
+
+//DISPLAY DRIVERR
+void vga_reset(uint8_t bg_color) 
+{
+	vga_row = 0;
+	vga_column = 0;
+	vga_color[0] = bg_color;
+	
+    //set constant color
+	for (size_t y = 0; y < VGA_HEIGHT; y++) {
+		for (size_t x = 0; x < VGA_WIDTH_B; x++) { //width /8
+			const size_t index = y * VGA_WIDTH_B + x;
+			vga_buffer[index] = 0xFF;
+		}
+	}
+}
+//==========================================================================================here
+void vga_set_pixel(uint8_t color, size_t x, size_t y) 
+{
+	const size_t index = y * VGA_WIDTH + x;
+	terminal_buffer[index] = vga_entry(c, color);
+}
+
+void terminal_putchar(char c) 
+{
+	terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
+	if (++terminal_column == VGA_WIDTH) {
+		terminal_column = 0;
+		if (++terminal_row == VGA_HEIGHT)
+			terminal_row = 0;
+	}
+}
+
+void terminal_write(const char* data, size_t size) 
+{
+	for (size_t i = 0; i < size; i++)
+		terminal_putchar(data[i]);
+}
+
+void terminal_writestring(const char* data) 
+{
+	terminal_write(data, strlen(data));
+}
